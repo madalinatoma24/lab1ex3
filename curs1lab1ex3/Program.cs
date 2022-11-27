3c59db5f43d7c63905dcf0215a3b557a3b407439
madalina.toma <madalina.toma@softem.ro>
2022-11-27 17:04:55 +0200

    Add project files.

---
+++ curs1lab1ex3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace curs1lab1ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+           Scrieti un program care va afisa ultima cifra a unui numar intreg citit de la tastaura
+           */
+
+            long number;
+            Console.WriteLine("Introduceti numarul: ");
+            number = long.Parse(Console.ReadLine());
+
+            long result = number %10;
+
+            Console.WriteLine("Ultima cifra este: " + result);
+        }
+    }
+}
